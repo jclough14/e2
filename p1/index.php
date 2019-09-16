a8029517eba4d4f0225e28324812120779e0bcc9
Jonathan Clough <jclough14@gmail.com>
2019-09-16 13:35:53 -0700

    p1 first attempt

--- p1/index.php
+++ p1/index.php
@@ -3,9 +3,51 @@
 <head>
 	<title>Practice Application</title>
  	<meta charset='utf-8'> 
+
+	<style>
+
+	body {
+		background-color: #FDD9B5;
+		width: 1920px;
+	}
+
+	h1  {
+		color: #000080;
+		width: 350px;
+		text-align: center;
+		margin: auto;
+	}
+	p   {
+		color: black;
+		text-align: center;
+		width: 350px;
+		margin: auto;
+		font-size: 18pt;
+	}
+
+	img {
+		width: 350px;
+		display: block;
+		margin-left: auto;
+		margin-right: auto;
+
+	}
+
+
+</style>
+
+
+
 </head> 
+
+
+
 <body> 	
-	<h1>Practice Application</h1> 
+	<h1>Jonathan Clough</h1> 
 	<img src='/images/CloughHeadshot.jpg'>
+
+	<p>Jonathan Clough is the Senior Assistant Dean of Finance and Administration for the School of Education and Counseling Psychology. He brings over 20 years of experience in higher education.
+Most recently, Jonathan served as the Senior Associate Director of Athletics at Santa Clara University. He has also worked at Stanford University and Villanova University.
+He holds master's degrees from Santa Clara University, Temple University and Villanova University. </P>
 </body> 
 </html>
