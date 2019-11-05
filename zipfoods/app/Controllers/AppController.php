9f6f52a70a153ed892feb4b3e8372a6206e530e4
Jonathan Clough <jclough14@gmail.com>
2019-11-05 09:41:51 -0800

    Zipfoods about routing for assignment 9

--- zipfoods/app/Controllers/AppController.php
+++ zipfoods/app/Controllers/AppController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Controllers;
 
 class AppController extends Controller
@@ -9,9 +10,15 @@ class AppController extends Controller
     public function index()
     {
         $welcomes = ['Welcome', 'Aloha!', 'Welkom', 'Bienvenidos', 'Bienvenu', 'Welkomma'];
-        
+
         return $this->app->view('index', [
             'welcome' => $welcomes[array_rand($welcomes)]
         ]);
+    }
+
+    public function about()
+    {
+
+        return 'zipfoods is your one-stop-shop for convenient online grocery shopping in the greater Boston area';
     }
 }
